11ea1beed269f9fa5ee447b3d48038bd46196a40
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-02-14 22:44:39 +0100

    Clean up

--- lib/format_parser.ex
+++ lib/format_parser.ex
@@ -16,34 +16,47 @@ defmodule FormatParser do
       <<"FLV", 0x01, x :: binary>> -> parse_flv(x)
       <<"GIF87a", x :: binary>> -> parse_gif(x)
       <<0xFF, 0xD8, 0xFF, x :: binary>> -> parse_jpeg(x)
-      <<0x49, 0x49, 0x2A, 0x00, 0x10, 0x00, 0x00, 0x00, 0x43, 0x52, x :: binary>> -> %Image{format: :cr2}
-      <<0x49, 0x49, 0x2A, 0x00, x :: binary>> -> %Image{format: :tif}
-      <<0x00, 0x00, 0x01, 0x00, x :: binary>> -> %Image{format: :ico}
-      <<0x7B, 0x5C, 0x72, 0x74, 0x66, 0x31, x :: binary>> -> %Document{format: :rtf}
-      <<0x4D, 0x4D, 0x00, 0x2A, x :: binary>> -> :tiff
-      <<0x80, 0x2A, 0x5F, 0xD7, x :: binary>> -> :cin
-      
-      true -> {:error, "Unknown"}
+      <<0x49, 0x49, 0x2A, 0x00, 0x10, 0x00, 0x00, 0x00, 0x43, 0x52, x :: binary>> -> parse_cr2(x)
+      <<0x49, 0x49, 0x2A, 0x00, x :: binary>> -> parse_tif(x)
+      <<0x00, 0x00, 0x01, 0x00, x :: binary>> -> parse_ico(x)
+      <<0x7B, 0x5C, 0x72, 0x74, 0x66, 0x31, x :: binary>> -> parse_rtf(x)
+      _ -> {:error, "Unknown"}
     end
   end
   
-  def parse_flac(<<x:: binary>>) do
+  def parse_rtf(<<x :: binary>>) do
+    %Document{format: :rtf}
+  end
+  
+  def parse_ico(<<_x:: binary>>) do
+    %Image{format: :ico}
+  end
+  
+  def parse_tif(<<_x:: binary>>) do
+    %Image{format: :tif}
+  end
+  
+  def parse_cr2(<<_x:: binary>>) do
+    %Image{format: :cr2}
+  end
+  
+  def parse_flac(<<_x:: binary>>) do
     %Audio{format: :flac}
   end
   
-  def parse_ogg(<<x:: binary>>) do
+  def parse_ogg(<<_x:: binary>>) do
     %Audio{format: :ogg}
   end
   
-  def parse_wav(<<_ :: size(144), channels :: little-integer-size(16), sample_rate_hz :: little-integer-size(32), x :: binary>>) do
+  def parse_wav(<<_ :: size(144), channels :: little-integer-size(16), sample_rate_hz :: little-integer-size(32), _x :: binary>>) do
     %Audio{format: :wav, sample_rate_hz: sample_rate_hz, num_audio_channels: channels}
   end
   
-  def parse_aiff(<<_ :: size(56), "COMM", _ :: size(96), sample_rate_hz :: size(80), x :: binary>>) do
+  def parse_aiff(<<_ :: size(56), "COMM", _ :: size(96), sample_rate_hz :: size(80), _x :: binary>>) do
     %Audio{format: :aiff, sample_rate_hz: sample_rate_hz}
   end
   
-  def parse_flv(x) do
+  def parse_flv(_x) do
     %Video{format: :flv}
   end
   
@@ -51,7 +64,7 @@ defmodule FormatParser do
     %Image{format: :gif, width_px: width, height_px: height}
   end
 
-  defp parse_jpeg(binary) do
+  defp parse_jpeg(_binary) do
     %Image{format: :jpg}
   end
 
