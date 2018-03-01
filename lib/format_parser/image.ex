240ee7d180f018fc2dbc0d8e622fbb3ca598bc83
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-03-01 21:29:59 +0100

    Boyscout

--- lib/format_parser/image.ex
+++ lib/format_parser/image.ex
@@ -43,7 +43,7 @@ defmodule FormatParser.Image do
       <<0x00, 0x00, 0x01, 0x00, x :: binary>> -> parse_ico(x)
       <<0x00, 0x00, 0x02, 0x00, x :: binary>> -> parse_cur(x)
       <<"8BPS", x :: binary>> -> parse_psd(x)
-      <<0x97, 0x4A, 0x42, 0x32, 0x0D, 0x0A, 0x1A, 0x0A, x :: binary>> -> parse_jb2(x)
+      <<0x97, "JB2", 0x0D, 0x0A, 0x1A, 0x0A, x :: binary>> -> parse_jb2(x)
       <<"gimp xcf", x :: binary>> -> parse_xcf(x)
       <<0x76, 0x2F, 0x31, 0x01, x :: binary>> -> parse_exr(x)
       _ -> {:error, file}
@@ -69,26 +69,50 @@ defmodule FormatParser.Image do
   defp parse_ico(<<_ :: size(16), width :: size(8), height :: size(8), num_color_palette :: size(8), 0x00, color_planes :: size(16), bits_per_pixel :: size(16), _ :: binary>>) do
     width_px = if width == 0, do: 256, else: width
     height_px = if height == 0, do: 256, else: height
-    intrinsics = %{num_color_palette: num_color_palette, color_planes: color_planes, bits_per_pixel: bits_per_pixel}
-    %Image{format: :ico, width_px: width_px, height_px: height_px, intrinsics: intrinsics}
+    intrinsics = %{
+      num_color_palette: num_color_palette,
+      color_planes: color_planes,
+      bits_per_pixel: bits_per_pixel
+    }
+    %Image{
+      format: :ico,
+      width_px: width_px,
+      height_px: height_px,
+      intrinsics: intrinsics
+    }
   end
 
   defp parse_cur(<<_ :: size(16), width :: size(8), height :: size(8), num_color_palette :: size(8), 0x00, hotspot_horizontal_coords :: size(16), hotspot_vertical_coords :: size(16), _ :: binary>>) do
     width_px = if width == 0, do: 256, else: width
     height_px = if height == 0, do: 256, else: height
-    intrinsics = %{num_color_palette: num_color_palette, hotspot_horizontal_coords: hotspot_horizontal_coords, hotspot_vertical_coords: hotspot_vertical_coords}
-    %Image{format: :cur, width_px: width_px, height_px: height_px, intrinsics: intrinsics}
+    intrinsics = %{
+      num_color_palette: num_color_palette,
+      hotspot_horizontal_coords: hotspot_horizontal_coords,
+      hotspot_vertical_coords: hotspot_vertical_coords
+    }
+    %Image{
+      format: :cur,
+      width_px: width_px,
+      height_px: height_px,
+      intrinsics: intrinsics
+    }
   end
 
   defp parse_tif(<< ifd0_offset :: little-integer-size(32), x :: binary >>) do
     ifd_0 = parse_ifd0(x, shift(ifd0_offset, 8), false)
     width = ifd_0[256].value
     height = ifd_0[257].value
-    make = parse_make_tag(x, shift(ifd_0[271][:value], 8), shift(ifd_0[271][:length], 0))
+    make = parse_make_tag(
+      x,
+      shift(ifd_0[271][:value], 8),
+      shift(ifd_0[271][:length], 0)
+    )
 
     cond do
-     Regex.match?(~r/canon.+/i, make) -> %Image{format: :cr2, width_px: width, height_px: height}
-     Regex.match?(~r/nikon.+/i, make) -> %Image{format: :nef, width_px: width, height_px: height}
+     Regex.match?(~r/canon.+/i, make) ->
+       %Image{format: :cr2, width_px: width, height_px: height}
+     Regex.match?(~r/nikon.+/i, make) ->
+       %Image{format: :nef, width_px: width, height_px: height}
      make == "" -> %Image{format: :tif, width_px: width, height_px: height}
     end
   end
@@ -97,13 +121,21 @@ defmodule FormatParser.Image do
     ifd_0 = parse_ifd0(x, shift(ifd0_offset, 8), true)
     width = ifd_0[256].value
     height = ifd_0[257].value
-    make = parse_make_tag(x, shift(ifd_0[271][:value], 8), shift(ifd_0[271][:length], 0))
-    if Regex.match?(~r/nikon.+/i, make), do: %Image{format: :nef}, else: %Image{format: :tif, width_px: width, height_px: height}
+    make = parse_make_tag(
+      x,
+      shift(ifd_0[271][:value], 8),
+      shift(ifd_0[271][:length], 0)
+    )
+    if Regex.match?(~r/nikon.+/i, make) do
+      %Image{format: :nef}
+    else
+      %Image{format: :tif, width_px: width, height_px: height}
+    end
   end
 
   defp parse_ifd0(<< x :: binary >>, offset, big_endian)  when big_endian == false do
-    <<_ :: size(offset), ifd_count :: little-integer-size(16), rest :: binary>> = x
-    ifds_sizes = ifd_count * 12 * 8
+    <<_ :: size(offset), ifdc :: little-integer-size(16), rest :: binary>> = x
+    ifds_sizes = ifdc * 12 * 8
     << ifd_set :: size(ifds_sizes), _ :: binary >> = rest
     parse_ifds(<< ifd_set :: size(ifds_sizes) >>, big_endian, %{})
   end
@@ -155,7 +187,19 @@ defmodule FormatParser.Image do
   end
 
   defp parse_png(<< _ :: size(32), "IHDR", width :: size(32), height :: size(32), bit_depth, color_type, compression_method, filter_method, interlace_method, crc :: size(32), _ :: binary >>) do
-    intrinsics = %{bit_depth: bit_depth, color_type: color_type, compression_method: compression_method, filter_method: filter_method, interlace_method: interlace_method, crc: crc}
-    %Image{format: :png, width_px: width, height_px: height, intrinsics: intrinsics}
+    intrinsics = %{
+      bit_depth: bit_depth,
+      color_type: color_type,
+      compression_method: compression_method,
+      filter_method: filter_method,
+      interlace_method: interlace_method,
+      crc: crc
+    }
+    %Image{
+      format: :png,
+      width_px: width,
+      height_px: height,
+      intrinsics: intrinsics
+    }
   end
 end
