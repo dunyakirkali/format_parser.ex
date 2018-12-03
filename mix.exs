7f12c0dbebed14f4e5bdb19bb1860f24f3fb85af
Dunya Kirkali <dunyakirkali@dMac-2.local>
2018-12-03 21:59:46 +0100

    Bump required elixir to ~> 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FormatParser.Mixfile do
     [
       app: :format_parser,
       version: "0.4.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       package: package(),
