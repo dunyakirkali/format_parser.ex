a03b24282e439262ed8f0edf066c6b54541f1203
Dunya Kirkali <dunyakirkali@gmail.com>
2025-03-15 10:19:11 +0100

    Add docs for audio.ex

--- lib/format_parser/audio.ex
+++ lib/format_parser/audio.ex
@@ -49,10 +49,17 @@ defmodule FormatParser.Audio do
     end
   end
 
+  @doc """
+  Parses an MP3 file and returns an Audio struct with format set to :mp3.
+  """
   defp parse_mp3(<<_::binary>>) do
     %Audio{format: :mp3}
   end
 
+  @doc """
+  Parses a FLAC file and returns an Audio struct with format set to :flac.
+  Extracts the sample rate and number of audio channels from the file.
+  """
   defp parse_flac(
          <<_::size(112), sample_rate_hz::size(20), num_audio_channels::size(3), _::size(5),
            _::size(36), _::binary>>
@@ -64,14 +71,16 @@ defmodule FormatParser.Audio do
     }
   end
 
-  defp parse_ogg(<<_::binary-size(29), "vorbis", x::binary>>) do
-    parse_vorbis(x)
-  end
+  @doc """
+  Parses an OGG file and determines if it is a Vorbis or Opus file.
+  """
+  defp parse_ogg(<<_::binary-size(29), "vorbis", x::binary>>), do: parse_vorbis(x)
+  defp parse_ogg(<<_::binary-size(28), "OpusHead", x::binary>>), do: parse_opus(x)
 
-  defp parse_ogg(<<_::binary-size(28), "OpusHead", x::binary>>) do
-    parse_opus(x)
-  end
-
+  @doc """
+  Parses a Vorbis file and returns an Audio struct with format set to :vorbis.
+  Extracts the sample rate, number of audio channels, and Vorbis version from the file.
+  """
   defp parse_vorbis(<<
          vorbis_version::size(32),
          channels::size(8),
@@ -90,6 +99,10 @@ defmodule FormatParser.Audio do
     }
   end
 
+  @doc """
+  Parses a WAV file and returns an Audio struct with format set to :wav.
+  Extracts the sample rate, number of audio channels, byte rate, block align, and bits per sample from the file.
+  """
   defp parse_wav(
          <<_::binary-size(10), channels::little-integer-size(16),
            sample_rate_hz::little-integer-size(32), byte_rate::little-integer-size(32),
@@ -110,6 +123,10 @@ defmodule FormatParser.Audio do
     }
   end
 
+  @doc """
+  Parses an AIFF file and returns an Audio struct with format set to :aiff.
+  Extracts the number of audio channels, number of frames, and bits per sample from the file.
+  """
   defp parse_aiff(
          <<_::size(56), "COMM", _::size(32), channels::size(16), frames::size(32),
            bits_per_sample::size(16), _sample_rate_components::size(80), _::binary>>
@@ -118,10 +135,17 @@ defmodule FormatParser.Audio do
     %Audio{format: :aiff, num_audio_channels: channels, intrinsics: intrinsics}
   end
 
+  @doc """
+  Parses an AAC file and returns an Audio struct with format set to :aac.
+  """
   defp parse_aac(<<_::binary>>) do
     %Audio{format: :aac}
   end
 
+  @doc """
+  Parses an Opus file and returns an Audio struct with format set to :opus.
+  Extracts the sample rate, number of audio channels, version, pre-skip, output gain, and mapping family from the file.
+  """
   defp parse_opus(<<
          version::integer-size(8),
          channels::integer-size(8),
