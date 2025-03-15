f1cecc8c68512f6de90dc8fcb4145c276feb4550
Dunya Kirkali <dunyakirkali@gmail.com>
2025-03-15 09:51:44 +0100

    Bump version to 2.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FormatParser.Mixfile do
   def project do
     [
       app: :format_parser,
-      version: "2.0.0",
+      version: "2.1.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
